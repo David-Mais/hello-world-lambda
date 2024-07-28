a0a201634826e2eddf2a65510e7e1e560d60dac9
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2024-07-28 21:11:31 +0400

    completed

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,10 +2,12 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,22 +21,28 @@ import java.util.Map;
 		authType = AuthType.NONE
 )
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+	private static final ObjectMapper objectMapper = new ObjectMapper();
 
 	@Override
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		Map<String, Object> response = new HashMap<>();
 
-		Map<String, Object> requestMap = (Map<String, Object>) request;
+		try {
+			Map<String, Object> requestMap = objectMapper.convertValue(request, Map.class);
 
-		String path = (String) requestMap.get("path");
-		String method = (String) requestMap.get("httpMethod");
+			String path = (String) requestMap.get("path");
+			String method = (String) requestMap.get("httpMethod");
 
-		if ("/hello".equals(path) && "GET".equals(method)) {
-			response.put("statusCode", 200);
-			response.put("message", "Hello from Lambda");
-		} else {
-			response.put("statusCode", 400);
-			response.put("message", String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, method));
+			if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+				response.put("statusCode", 200);
+				response.put("message", "Hello from Lambda");
+			} else {
+				response.put("statusCode", 400);
+				response.put("message", String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, method));
+			}
+		} catch (Exception e) {
+			response.put("statusCode", 500);
+			response.put("message", "Internal server error.");
 		}
 
 		return response;
