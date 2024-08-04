29d81c45d074b9634c752a600926035b8dd30f01
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2024-08-04 20:52:10 +0400

    completed

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -4,7 +4,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.RegionScope;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
@@ -19,6 +21,10 @@ import java.util.Map;
 @SnsEventSource(
 		targetTopic = "lambda_topic",
 		regionScope = RegionScope.DEFAULT
+)
+@DependsOn(
+		name = "lambda_topic",
+		resourceType = ResourceType.SNS_TOPIC
 )
 public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
 
