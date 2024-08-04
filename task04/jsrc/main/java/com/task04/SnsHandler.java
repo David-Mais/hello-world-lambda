efb4e92e943a387b75c592041a3f8abde99be5d3
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2024-08-04 21:18:13 +0400

    completed

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -14,8 +14,8 @@ import java.util.Map;
 
 @LambdaHandler(lambdaName = "sns_handler",
 	roleName = "sns_handler-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
+	isPublishVersion = false,
+//	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @SnsEventSource(
