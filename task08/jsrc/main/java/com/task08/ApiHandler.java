a74969ab9693ea35fc05b30c50e6e8e3cccdf216
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2024-08-15 02:18:50 +0400

    completed

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -7,6 +7,7 @@ import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
@@ -30,7 +31,8 @@ import java.util.Map;
 		layerName = "openmetroclient-layer",
 		libraries = "lambda_layer/java/lib/task08-1.0.0.jar",
 		runtime = DeploymentRuntime.JAVA11,
-		architectures = Architecture.ARM64
+		architectures = Architecture.ARM64,
+		artifactExtension = ArtifactExtension.JAR
 )
 public class ApiHandler implements RequestHandler<Map<String, Object>, String> {
 	public String handleRequest(Map<String, Object> request, Context context) {
